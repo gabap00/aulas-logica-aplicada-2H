fd0347a1136cdc89cc963650b4c8aee4d8f9aa62
gabap00 <kratosnaopula@gmail.com>
2023-05-25 11:05:44 -0300

    retomada de 00 sobrescrita

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        int contador = 0;
-        // laço de repetição
-        while(contador < 10){
-            System.out.println("Contador = " + contador);
-            // incremento
-            contador = contador + 1;
-        }
+        // Aula introdutória de Orientação à Objetos
+// Declaração de objeto
+        Pessoa adao;
+// Instanciação do objeto
+        adao = new Pessoa();
+// Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+// Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
 
-        // laço para faça
-        for(int x=0; x < 10; x++){
-            System.out.println("Contador = " + x);
-        }
 
-        // laço faça enquanto
-        int cont = 1;
-        do{
-            // processo
-            System.out.println(cont);
-            // incremento
-            cont++;
-        }while(cont <= 10);
+
+        //Criar novos objetos
+
+        Pessoa rainha;
+        //Instanciar objetos
+        rainha = new Pessoa();
+        //Definir forma do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+        // Definição de comportamneto
+        rainha.falar();
+        rainha.comer();
+
+
 
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa extends Animal {
+
+
+    // Atributos da classe
+
+    public String sobrenome;
+    // métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;  }
+
+    public void comer() {
+        System.out.println("pessoa comeu");
+    }
+}
+
+
+
