c0cb391b8843affceb1a0e63cb1596602fdf00da
Gabriel Pereira <rm88451@estudante.fieb.edu.br>
2023-06-14 09:07:20 -0300

    aula de logica xd

---
+++ src/Planta.java
@@ -0,0 +1,12 @@
+public class Planta {
+    // Construtor Padrão
+    Planta(){
+
+
+    }
+    //Sobrecarga do método construtor
+    Planta(Semente semente){
+
+    }
+
+}
---
+++ src/Solo.java
@@ -0,0 +1,13 @@
+public class Solo      {
+    //atributos
+    String tiposolo;
+    String cro;
+    boolean fertl;
+    //Método
+
+    public Planta gerarPlanta(Semente s1){
+        Planta p1 = new Planta(s1);
+        return p1;
+    }
+
+}
